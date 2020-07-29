63c0d7ea619e532396a33a23d41cc85a201f94d8
chemotaxis <ensminger.ethan@gmail.com>
2020-07-28 23:57:27 -0400

    Draft animation
    I added the animation from the repo I used to test out Framer Motion.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import React from 'react';
 import './App.css';
 
+import { motion } from 'framer-motion';
+
+const howMuchSlower = 5;
+
+// Measured in seconds
+const fastPlaneSpeed = 1;
+const slowPlaneSpeed = fastPlaneSpeed*howMuchSlower;
+
+// Measured in pixels
+const leftBound = -200;
+const rightBound = 200;
+const altitude = 100;
+
 function App() {
   return (
     <div className="App">
       <h1>Speed Visualization</h1>
+      <div className="Animation">
+        <motion.div className="circle"
+          animate={{
+            x: [leftBound, rightBound],
+            y: [0, -altitude, 0],
+          }}
+          transition={{
+            ease: "easeInOut",
+            flip: howMuchSlower - 1,
+            duration: fastPlaneSpeed,
+          }}
+        />
+        <div className="ring"
+          style={{
+            position: "relative",
+            left: `${leftBound}px`,
+            top: "-80px",
+          }}
+        />
+        <div className="ring"
+          style={{
+            position: "relative",
+            right: `-${rightBound}px`,
+            top: "-160px",
+          }}
+        />
+        <motion.div className="circle"
+          style={{
+            position: "relative",
+            top: "-240px",
+          }}
+          animate={{
+            x: [leftBound, rightBound],
+            y: [0, altitude, 0]
+          }}
+          transition={{
+            ease: "easeInOut",
+            duration: slowPlaneSpeed,
+            flip: 0,
+          }}
+        />
+      </div>
     </div>
   );
 }
