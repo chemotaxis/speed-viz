08921e39a1cb295493459717191c2a182b28e6b8
chemotaxis <ensminger.ethan@gmail.com>
2020-07-30 12:50:28 -0400

    Play around with composing containers
    This is my first attempt.  Left and right sides are separate.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 import { motion } from 'framer-motion';
@@ -10,56 +11,74 @@ const fastPlaneSpeed = 1;
 const slowPlaneSpeed = fastPlaneSpeed*howMuchSlower;
 
 // Measured in pixels
-const leftBound = -200;
-const rightBound = 200;
+const leftBound = 0;
+const rightBound = 0.6*document.documentElement.clientWidth - 40;
 const altitude = 100;
 
+function Button({onClick}) {
+  return (
+    <button onClick={onClick}>
+      Run again
+    </button>
+  )
+}
+
+function Animation() {
+  return (
+    <div className="Animation">
+      <div className="leftContainer">
+        <div className="container">
+          <div className="yellowBox"></div>
+          <div className="planeName">Mach 5</div>
+        </div>
+        <div className="container">
+          <div className="box"></div>
+          <div className="cityLabel">NY</div>
+          <motion.div className="circle"
+            animate={{
+              x: [leftBound, rightBound],
+              y: [0, -altitude, 0],
+            }}
+            transition={{
+              ease: "easeInOut",
+              flip: howMuchSlower - 1,
+              duration: fastPlaneSpeed,
+            }}
+          />
+          <div className="ring"
+            style={{
+              position: "relative",
+              right: "40px",
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="ring"/>
+      <motion.div className="circle"
+        style={{
+          position: "absolute",
+        }}
+        animate={{
+          x: [leftBound, rightBound],
+          y: [0, altitude, 0]
+        }}
+        transition={{
+          ease: "easeInOut",
+          duration: slowPlaneSpeed,
+          flip: 0,
+        }}
+      />
+    </div>
+  )
+}
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>Speed Visualization</h1>
-      <div className="Animation">
-        <motion.div className="circle"
-          animate={{
-            x: [leftBound, rightBound],
-            y: [0, -altitude, 0],
-          }}
-          transition={{
-            ease: "easeInOut",
-            flip: howMuchSlower - 1,
-            duration: fastPlaneSpeed,
-          }}
-        />
-        <div className="ring"
-          style={{
-            position: "relative",
-            left: `${leftBound}px`,
-            top: "-80px",
-          }}
-        />
-        <div className="ring"
-          style={{
-            position: "relative",
-            right: `-${rightBound}px`,
-            top: "-160px",
-          }}
-        />
-        <motion.div className="circle"
-          style={{
-            position: "relative",
-            top: "-240px",
-          }}
-          animate={{
-            x: [leftBound, rightBound],
-            y: [0, altitude, 0]
-          }}
-          transition={{
-            ease: "easeInOut",
-            duration: slowPlaneSpeed,
-            flip: 0,
-          }}
-        />
-      </div>
+      <Animation key={count}/>
+      <Button onClick={() => setCount(count?0:1)}/>
     </div>
   );
 }
