9b2bafc38b536259ad993ab9629dc0f3661be1d6
chemotaxis <chemotaxis@users.noreply.github.com>
2020-08-05 17:21:48 -0400

    Clean up image import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
-import * as src from './static/hermeus-thumbnail.jpg';
+import src from './static/hermeus-thumbnail.jpg';
 import { motion, useAnimation } from 'framer-motion';
 
 
